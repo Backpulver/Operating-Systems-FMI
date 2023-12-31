cee2f6b7af580ffa5cfea3ac532f654819765455
joni <joni@tostera.tostera>
2023-12-31 16:14:01 +0200

    Update scripts

--- Collection of tasks/Linux shell/Scripts/18/2016-SE-03.sh
+++ Collection of tasks/Linux shell/Scripts/18/2016-SE-03.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-if [[ $(whoami) == "root" ]]; then
-    while IFS= read -r line; do
-        echo
-    done
-else
+if ! [[ $(whoami) == "root" ]]; then
     echo "Not a root user!" 
     exit 1
 fi
+
+while IFS= read -r line; do
+    username=$(echo $line | cut -d':' -f1)
+    homedir=$(echo $line | cut -d':' -f6)
+
+    if [[ -z "$homedir" ]] || [[ ! -d "$homedir" ]]; then
+        echo "User $username does not have a home directory"
+    else
+        if [[ ! -w "$homedir" ]]; then
+            echo "User $username does not have write permissions for his home directory"
+        fi
+    fi
+done < /etc/passwd
