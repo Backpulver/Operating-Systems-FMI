0f796db28a08d303f0ae0ab73095cf6b353b3c09
Backpulver <joni@tostera.tostera>
2025-01-12 16:33:34 +0200

    Remove unnecessary files from repo

--- Repo/05.scripts/Code/dirItems.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo -n "Enter directory path: "
-read -r dirPath
-
-filesCount=$(find "$dirPath" -mindepth 1 -maxdepth 1 -type f | wc -l)
-dirCount=$(find "$dirPath" -mindepth 1 -maxdepth 1 -type d | wc -l)
-
-echo -e "Files: $filesCount\nDirectories: $dirCount"
