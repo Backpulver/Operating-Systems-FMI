b2d23cf50442e2e4e7f5ffcdf1646e6f94382a4b
Backpulver <yoanhristov9@gmail.com>
2025-08-18 22:06:24 +0300

    Fix shell 2023-CE-02

--- Exams/Shell/Scripts/2023-CE-02/2023-CE-02.sh
+++ Exams/Shell/Scripts/2023-CE-02/2023-CE-02.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
 
-if [[ ${#} -ne 3 ]]; then
-	echo "Err" 
+if [ "$#" -ne 3 ]; then
+	echo "Usage: ${0} <Data file 1> <Data file 2> <Black hole name>"  >&2
+	exit 1
 fi
 
-zastoqnie1=$(grep -Eo "^${3}: [1-9][0-9]* megaparsecs$" "${1}")
-echo "$zastoqnie1"
+if [ ! -f "${1}" ]; then
+	echo "${1} is not a valid file" >&2
+	exit 2
+fi
+
+if [ ! -f "${2}" ]; then
+	echo "${2} is not a valid file" >&2
+	exit 3
+fi
+
+distance1=$(grep -Eo "^.+: (0|[1-9][0-9]*) megaparsecs$" "${1}" | grep "^${3}:" | cut -d':' -f2 | cut -d' ' -f2)
+distance2=$(grep -Eo "^.+: (0|[1-9][0-9]*) megaparsecs$" "${2}" | grep "^${3}:" | cut -d':' -f2 | cut -d' ' -f2)
+
+answerFile=""
+
+if [ -z "${distance1}" ] && [ -z "${distance2}" ]; then
+	echo "No point contain the name of the black hole" >&2
+	exit 4
+elif [ -z "${distance1}" ]; then
+	answerFile="${2}"
+elif [ -z "${distance2}" ]; then
+	answerFile="${1}"
+elif [ "${distance1}" -le "${distance2}" ]; then
+	answerFile="${1}"
+else
+	answerFile="${2}"
+fi
+
+basename "${answerFile}"
